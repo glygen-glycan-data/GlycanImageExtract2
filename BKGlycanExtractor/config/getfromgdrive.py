e08c14fa62f80e0bd6e00246079488553a1ec255
Nathan Edwards <edwardsnj@gmail.com>
2025-04-22 17:23:53 -0400

    fix get from gdrive

--- BKGlycanExtractor/config/getfromgdrive.py
+++ BKGlycanExtractor/config/getfromgdrive.py
@@ -1,24 +1,67 @@
+#!/bin/env python3
 #Reference: https://stackoverflow.com/questions/38511444/python-download-files-from-google-drive-using-url
 
-import requests
+import requests, re, json, os
+
+def download_fileids_from_google_drive(fid, destination_dir,level=0):
+    URL = "https://drive.google.com/drive/u/2/folders/"
+    session = requests.Session()
+
+    response = session.get(URL + fid)
+
+    files = dict()
+    dirs = dict()
+
+    data = re.sub(r'(</[a-zA-Z]+>)',r'\1\n',response.text)
+    for l in data.splitlines():
+        if 'window[\'_DRIVE_ivd\'] = ' in l:
+            l = eval(l.split('window[\'_DRIVE_ivd\'] = ',1)[1].split(';if ',1)[0].replace(r'\/','/'))
+            data = json.loads(l)
+            data = data[0]
+            for i,it in enumerate(data):
+                # print(it)
+                itdata = dict(id=it[0],name=it[2],type=it[3],size=it[13],ordinal=i+1)
+                if 'folder' in it[3]:
+                    dirs[i] = itdata
+                else:
+                    files[i] = itdata
+
+    for k in sorted(dirs):
+        print("%s%s/"%(" "*level*2,dirs[k]['name']))
+        download_fileids_from_google_drive(dirs[k]['id'],dirs[k]['name'],level=level+1)
+
+    for k in sorted(files):
+        f = files[k]
+        print("%s%s (%d bytes)..."%(" "*level*2,f['name'],f['size']),end=" ")
+        sys.stdout.flush()
+        os.makedirs(destination_dir,exist_ok=True)
+        filepath = os.path.join(destination_dir,f['name'])
+        if os.path.exists(filepath) and os.path.getsize(filepath) == f['size']:
+            print("found.")
+            continue
+        download_file_from_google_drive(f['id'],filepath)
+        print("downloaded.")
 
 def download_file_from_google_drive(id, destination):
-    URL = "https://docs.google.com/uc?export=download"
+    URL = "https://drive.usercontent.google.com/download"
 
     session = requests.Session()
 
-    response = session.get(URL, params = { 'id' : id }, stream = True)
+    params = { 'id' : id, 'export': 'download' }
+    # print(URL, params)
+    response = session.get(URL, params = params, stream = True)
     token = get_confirm_token(response)
 
     if token:
-        params = { 'id' : id, 'confirm' : token }
+        params['confirm'] = token
+        # print(URL, params)
         response = session.get(URL, params = params, stream = True)
 
     save_response_content(response, destination)    
 
 def get_confirm_token(response):
     if "Virus scan warning" in response.text:
-        return 1
+        return 't'
     return None
 
 def save_response_content(response, destination):
@@ -30,6 +73,12 @@ def save_response_content(response, destination):
                 f.write(chunk)
 
 if __name__ == "__main__":
-    file_id = 'TAKE ID FROM SHAREABLE LINK'
-    destination = 'DESTINATION FILE ON YOUR DISK'
-    download_file_from_google_drive(file_id, destination)
+
+    import sys
+    folder_id = '1cK7xwAKl5jwezDBZRUDyYVltVHv1NsRf'
+    dest_dir = '.'
+    if len(sys.argv) >= 2:
+        folder_id = sys.argv[1]
+    if len(sys.argv) >= 3:
+        dest_dir = sys.argv[2]
+    download_fileids_from_google_drive(folder_id, dest_dir)
