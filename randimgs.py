4d3cef1fd8435c6513fe684c86e430353364831d
Nathan Edwards <edwardsnj@gmail.com>
2025-07-10 20:01:34 -0400

    restrict valid parent bond positions

--- randimgs.py
+++ randimgs.py
@@ -184,6 +184,9 @@ for j in range(iterations):
             if pp != None and len(pp) > 1:
                 bad = True
                 break
+            if list(pp)[0] not in (2,3,4,6,8):
+                bad = True
+                break
         if bad:
             continue
         if randmode in ("mono","biasmono"):
