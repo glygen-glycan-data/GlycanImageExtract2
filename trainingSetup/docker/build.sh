ea88eeeb95b5645dd30c46e64d49014cea16640e
Nathan Edwards <edwardsnj@gmail.com>
2025-10-31 10:52:45 -0400

    fix bad checkin

--- trainingSetup/docker/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-DATE=`date +%F`
-docker build --tag glyomics/darknet:$DATE -tag glyomics/darknet:latest --build-arg build_date=$DATE .
-docker push glyomics/darknet:$DATE
-docker push glyomics/darknet:latest
--- trainingSetup/docker/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /src/darknet
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j8 
