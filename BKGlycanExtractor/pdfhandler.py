a3a889b108c184fca3ddc46e8abdeb9e5881c904
Nathan Edwards <edwardsnj@gmail.com>
2025-11-05 11:03:32 -0500

    Inital PDF handler class

---
+++ BKGlycanExtractor/pdfhandler.py
@@ -0,0 +1,113 @@
+
+import fitz, os, os.path
+
+class PDFHandler(object):
+    def __init__(self,filename):
+        self.doc = fitz.open(filename)
+        self.dir,self.base = os.path.split(filename)
+        self.base,self.extn = self.base.rsplit('.',1)
+    
+    def make_figure_filename(self,image):
+        return os.path.join(self.dir,self.base+"-"+str(image['xref'])+".png")
+
+    def pages(self):
+        return self.doc.pages()
+    
+    def images_per_page(self,page):
+        return page.get_image_info(xrefs=True)
+    
+    @staticmethod
+    def image_dimensions(image_info):
+        # Why do we need to create a box?
+        pdf_fig_box = fitz.Rect(image_info['bbox'])
+        pdf_fig_height = pdf_fig_box.height
+        pdf_fig_width = pdf_fig_box.width
+        return pdf_fig_width,pdf_fig_height
+    
+    @staticmethod
+    def image_bbox(image_info):
+        # Why do we need to create a box?
+        pdf_fig_box = fitz.Rect(image_info['bbox'])
+        return pdf_fig_box.x0, pdf_fig_box.y0, pdf_fig_box.width, pdf_fig_box.height
+
+    @staticmethod
+    def create_box(bbox):
+        return fitz.Rect(bbox)
+    
+    def write_image(self,image,filename=None):
+        if filename is None:
+            filename = self.make_figure_filename(image)
+        pic = fitz.Pixmap(self.doc, image['xref'])
+        failed = False
+        try:
+            pic.save(filename)
+        except ValueError:
+            failed = True
+        if failed:
+            # If save fails, try to convert to RGB and try again
+            pic = fitz.Pixmap(fitz.csRGB, pic)
+            pic.save(filename)
+    
+    def figures(self,filter=None):
+        for page_number,page in enumerate(self.pages(),1):
+            images = self.images_per_page(page)
+            for image_number,image in enumerate(images,1):
+                image['page_number'] = page_number
+                image['image_number'] = image_number
+                image['pdf_fig_bbox'] = self.image_bbox(image)
+                if filter is None or filter.keep(image):
+                    yield image
+
+class PDFImageFilter(object):
+    def keep(self,image):
+        raise NotImplementedError
+
+class CompoundPDFImageFilter(PDFImageFilter):
+    def __init__(self,*filters):
+        self._filters = filters
+
+    def keep(self,image):
+        for f in self._filters:
+            if not f.keep(image):
+                return False
+        return True
+
+class PDFXRefImageFilter(PDFImageFilter):
+    def __init__(self,min_xref=1):
+        self._min_xref = min_xref
+
+    def keep(self,image):
+        if image['xref'] >= self._min_xref:
+            return True
+        return False
+
+class PDFImageSizeFilter(PDFImageFilter):
+    def __init__(self,width=90,height=90,area=None):
+        self._width = width
+        self._height = height
+        if area is not None:
+            self._area = area
+        else:
+            self._area = width*height
+    
+    def keep(self,image):
+        width,height = PDFHandler.image_dimensions(image)
+        area = width*height;
+        if width >= self._width and height >= self._height:
+            return True
+        if area >= self._area:
+            return True
+        return False
+
+if __name__ == "__main__":
+
+    import sys
+
+    pdf = PDFHandler(sys.argv[1])
+    filter = CompoundPDFImageFilter(
+        PDFXRefImageFilter(min_xref=1),
+        PDFImageSizeFilter(width=90,height=90)
+        )
+    for fig in pdf.figures(filter=filter):
+        print(fig)
+        pdf.write_image(fig)
