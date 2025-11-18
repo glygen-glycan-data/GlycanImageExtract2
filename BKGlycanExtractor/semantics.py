370b535e19689bb877e656a500bf807dd3bad79b
Nathan Edwards <edwardsnj@gmail.com>
2025-11-18 10:25:24 -0500

    Make annotate boxes more flexible

--- BKGlycanExtractor/semantics.py
+++ BKGlycanExtractor/semantics.py
@@ -422,14 +422,18 @@ class FigureSemantics(ImageSemantics):
                     text = str(link.classlabel())
                 self.annotate(x_min,y_min,x_max,y_max,color=color,text=text,thickness=1)
 
-    def annotate_boxes(self,boxes,colors=[(255, 255, 0)],labels=False):
+    def annotate_boxes(self,boxes,colors=None,color=(255, 255, 0),labels=False,label=None,anchor="TR"):
         for box in boxes:
             x_min,y_min,x_max,y_max = box.corners()
             text=''
+            if label:
+                text = label
             if labels:
                 text = str(box.get('classlabel',''))
+            if not colors:
+                colors = [ color ]
             color = colors[box.get('classid',0)%len(colors)]
-            self.annotate(x_min,y_min,x_max,y_max,color=color,text=text,thickness=1)
+            self.annotate(x_min,y_min,x_max,y_max,color=color,text=text,thickness=1,textanchor=anchor)
 
     def write_image(self,**kwargs):
         cv2.imwrite(self.make_filename(**kwargs), self.image())
